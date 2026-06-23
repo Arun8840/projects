7053ed30b2349745d35827dbd59fe8ae1e058fbf
Arun Prakash <arun.prabhakaran@skystack.io>
2026-06-23 20:09:20 +0530

    chore(app): update root layout and home page

--- apps/workflow/app/page.tsx
+++ apps/workflow/app/page.tsx
@@ -1,13 +1,13 @@
-import { Button } from "@repo/ui/button"
-import { Sparkle } from "lucide-react"
+import { Button } from '@repo/ui/button';
+import { Sparkle } from 'lucide-react';
 
 export default function Page() {
   return (
     <main className="bg-white min-h-screen flex justify-center items-center">
-      <Button size={"sm"}>
+      <Button size={'sm'}>
         <Sparkle />
         hello world
       </Button>
     </main>
-  )
+  );
 }
